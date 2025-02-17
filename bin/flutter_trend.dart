1ab370b5d3f1d9a9b89582a961ce62156fe6013d
imajoriri <imajo.srroni@gmail.com>
2025-02-18 08:46:51 +0900

    Update logging library and enhance GitHub issue tracking script

--- bin/flutter_trend.dart
+++ bin/flutter_trend.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_trend/model/github_color.dart';
 import 'package:flutter_trend/model/github_issue.dart';
 import 'package:flutter_trend/model/github_state.dart';
+import 'package:logger/web.dart';
 
 final dio = Dio();
 
@@ -14,14 +15,54 @@ final token = Platform.environment['SLACK_TOKEN'];
 /// GitHubのtokenを設定
 final githubToken = Platform.environment['PVT_GITHUB_TOKEN'];
 
+/// ログをデバッグでも表示するためのクラス。
+// github actionsで使う想定のため問題ない。
+class MyFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    return true;
+  }
+}
+
+final log = Logger(
+  printer: PrettyPrinter(),
+  output: ConsoleOutput(),
+  level: Level.all,
+  filter: MyFilter(),
+);
+
 void main(List<String> arguments) async {
   // 1時間前
-  final oneHourAgo = DateTime.now().subtract(Duration(hours: 24));
+  final oneHourAgo = DateTime.now().toUtc().subtract(Duration(hours: 1));
+  print(oneHourAgo);
 
   final queries = [
     GitHubIssueQuery(
-      slackChannel: '#flutter',
-      // label: 'f: cupertino',
+      slackChannel: '#repo-material-design',
+      label: 'f: material design',
+      since: oneHourAgo,
+      state: GitHubState.open,
+    ),
+    GitHubIssueQuery(
+      slackChannel: '#repo-cupertino',
+      label: 'f: cupertino',
+      since: oneHourAgo,
+      state: GitHubState.open,
+    ),
+    GitHubIssueQuery(
+      slackChannel: '#repo-animation',
+      label: 'a: animation',
+      since: oneHourAgo,
+      state: GitHubState.open,
+    ),
+    GitHubIssueQuery(
+      slackChannel: '#repo-animation',
+      label: 'p: animations',
+      since: oneHourAgo,
+      state: GitHubState.open,
+    ),
+    GitHubIssueQuery(
+      slackChannel: '#repo-pull-request-all',
       since: oneHourAgo,
       state: GitHubState.open,
     ),
@@ -29,13 +70,28 @@ void main(List<String> arguments) async {
 
   for (final query in queries) {
     final url = query.generateUrl();
-    print(url);
+    log.i(url);
 
-    final response = await dio.get(url);
+    late final Response response;
+    try {
+      response = await dio.get(
+        url,
+        options: Options(headers: {'Authorization': 'Bearer $githubToken'}),
+      );
+    } on DioException catch (e) {
+      log.e(e);
+      continue;
+    }
     final issues =
         (response.data as List).map((e) => GitHubIssue.fromJson(e)).toList();
 
     for (final issue in issues) {
+      // 1時間前に作成されたissueで、1時間前に閉じられたissueは除外
+      if (issue.createdAt.isBefore(oneHourAgo) &&
+          (issue.closedAt?.isBefore(oneHourAgo) ?? true)) {
+        continue;
+      }
+      log.i(issue.title);
       final attachments = [
         {
           'color':
@@ -58,15 +114,16 @@ void main(List<String> arguments) async {
         'attachments': attachments,
       };
 
-      print(token);
-
-      final response = await dio.post(
-        'https://slack.com/api/chat.postMessage',
-        data: jsonEncode(payload),
-        options: Options(headers: {'Authorization': 'Bearer $token'}),
-      );
-
-      print(response.data);
+      try {
+        final response = await dio.post(
+          'https://slack.com/api/chat.postMessage',
+          data: jsonEncode(payload),
+          options: Options(headers: {'Authorization': 'Bearer $token'}),
+        );
+        log.i(response.data);
+      } on DioException catch (e) {
+        log.e(e);
+      }
     }
   }
 }
@@ -91,7 +148,6 @@ class GitHubIssueQuery {
       'labels': label,
       'since': since.toIso8601String(),
       'state': state.name,
-      'per_page': '1',
     };
     final queryString = params.entries
         .map(
